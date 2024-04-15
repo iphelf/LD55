e828d0bd1de3541ef5c29a9e9b5fe5d62bd12282
Lingfeng Fu <2871618714@qq.com>
2024-04-16 00:42:31 +0800

    体育老师

--- Assets/Summons/Scripts/ViewCtrls/Places/CramSchoolCtrl.cs
+++ Assets/Summons/Scripts/ViewCtrls/Places/CramSchoolCtrl.cs
@@ -1,32 +1,25 @@
 using Summons.Scripts.Managers;
 using Summons.Scripts.Models;
+using Summons.Scripts.ViewCtrls.GameModules;
 using Summons.Scripts.ViewCtrls.MiniGames.BallGame;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Summons.Scripts.ViewCtrls.Places
 {
     public class CramSchoolCtrl : PlaceCtrlBase
     {
-        // Start is called before the first frame update
-        [SerializeField] private GameObject BallGameNPC;
-
+        [SerializeField] private NpcCtrl sportsTeacher;
         [SerializeField] private BallGameCtrl ballGameCtrl;
 
-        [SerializeField] private Button ballstart;
-
-        //[SerializeField] private Button hideball;
         private QuestInfo _running; // 若不为null，则小游戏正在运行中，且执行的是该任务
 
         private void Start()
         {
             ballGameCtrl.gameObject.SetActive(false);
 
-            ballstart.onClick.AddListener(() =>
+            sportsTeacher.onSummonRespond.AddListener(quest =>
             {
                 if (_running != null) return;
-                var quest = QuestManager.GetNextOngoingQuestOfType(QuestType.PracticeVolleyball);
-                if (quest == null) return;
                 _running = quest;
                 ballGameCtrl.gameObject.SetActive(true);
                 ballGameCtrl.Setup(quest.Args, () =>
@@ -35,66 +28,14 @@ namespace Summons.Scripts.ViewCtrls.Places
                     QuestManager.EndQuest(quest.Id);
                     _running = null;
                 });
-                QuestManager.OnQuestEnd.AddListener(OnQuestEnd);
             });
+            QuestManager.OnQuestEnd.AddListener(OnQuestEnd);
         }
 
         private void Update()
         {
-            //DetectQuestForStartButton();
-            bool showsBallEntry = false;
-            if (_running == null)
-            {
-                foreach (var id in QuestManager.OngoingQuests)
-                {
-                    var quest = QuestManager.GetQuestInfo(id);
-                    showsBallEntry = showsBallEntry || quest.Type == QuestType.PracticeVolleyball;
-                }
-            }
-
-            ballstart.gameObject.SetActive(showsBallEntry);
-        }
-
-        private void DetectQuestForStartButton()
-        {
-            if (QuestManager.OngoingQuests.Count == 0)
-            {
-                ballstart.gameObject.SetActive(false);
-                return;
-            }
-
-            var questInfo = QuestManager.GetNextOngoingQuestOfType(QuestType.PracticeVolleyball);
-            if (questInfo == null || questInfo.Type != QuestType.PracticeVolleyball)
-            {
-                ballstart.gameObject.SetActive(false);
-                return;
-            }
-
-            if (_running == null) ballstart.gameObject.SetActive(true);
-            else ballstart.gameObject.SetActive(false);
-        }
-
-        private QuestInfo DetectQuestForMiniGame()
-        {
-            if (QuestManager.OngoingQuests.Count == 0) return null;
-            var questInfo = QuestManager.GetNextOngoingQuestOfType(QuestType.PracticeVolleyball);
-            if (questInfo == null) return null;
-            return questInfo;
-        }
-
-        public override void OnEnterPlace(PlaceState state = null)
-        {
-            if (state is PlaceStateOfDemoPlace placeStateOfDemoPlace)
-            {
-                var isPresent = placeStateOfDemoPlace.IsPresent;
-            }
-        }
-
-        public override PlaceState OnExitPlace()
-        {
-            return new PlaceStateOfDemoPlace
-            {
-            };
+            bool allowInput = _running == null;
+            sportsTeacher.Interactable = allowInput;
         }
 
         private void OnDestroy()
