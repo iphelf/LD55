26da21b2552b569c99b1704a423fd0005a70468b
Lingfeng Fu <2871618714@qq.com>
2024-04-16 07:20:16 +0800

    结局

--- Assets/Summons/Scripts/SceneCtrls/EndCtrl.cs
+++ Assets/Summons/Scripts/SceneCtrls/EndCtrl.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using Summons.Scripts.Managers;
 using TMPro;
 using UnityEngine;
@@ -7,6 +8,10 @@ namespace Summons.Scripts.SceneCtrls
 {
     public class EndCtrl : MonoBehaviour
     {
+        [SerializeField] private GameObject buttonGroup;
+        [SerializeField] private TMP_Text conclusion;
+        [TextArea] [SerializeField] private string goodConclusion;
+        [TextArea] [SerializeField] private string badConclusion;
         [SerializeField] private Button restartButton;
         [SerializeField] private Button menuButton;
         [SerializeField] private Button quitButton;
@@ -14,7 +19,7 @@ namespace Summons.Scripts.SceneCtrls
         [SerializeField] private GameObject goodEndImage;
         [SerializeField] private GameObject badEndImage;
 
-        private void Start()
+        private async void Start()
         {
             restartButton.onClick.AddListener(GameManager.RestartGame);
             menuButton.onClick.AddListener(GameManager.ReturnToTitle);
@@ -23,9 +28,26 @@ namespace Summons.Scripts.SceneCtrls
             string scoreText = $"HP={GameManager.LastGameHp}, " +
                                $"Duration={Mathf.CeilToInt(GameManager.LastGameDuration)}s";
             score.text = scoreText;
+
             bool isGoodEnding = GameManager.LastGameHp > 0;
-            goodEndImage.SetActive(isGoodEnding);
-            badEndImage.SetActive(!isGoodEnding);
+
+            if (isGoodEnding)
+            {
+                goodEndImage.SetActive(true);
+                conclusion.text = goodConclusion;
+                await Awaitable.WaitForSecondsAsync(3.0f);
+                goodEndImage.SetActive(false);
+                await Awaitable.WaitForSecondsAsync(3.0f);
+            }
+            else
+            {
+                conclusion.text = badConclusion;
+                await Awaitable.WaitForSecondsAsync(3.0f);
+                badEndImage.SetActive(true);
+                await Awaitable.WaitForSecondsAsync(3.0f);
+            }
+
+            buttonGroup.SetActive(true);
         }
     }
 }
